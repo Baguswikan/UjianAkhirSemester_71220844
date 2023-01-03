e69ac6ca4dbfd387b58bcd7748b0347f9bc23137
Baguswikan <117887575+Baguswikan@users.noreply.github.com>
2023-01-03 12:09:27 +0700

    Add files via upload

---
+++ UAS_71220844_Bagus Wikan YS.py
@@ -0,0 +1,38 @@
+print('********** Kredit Keaktifan Mahasiswa **********')
+print('(Student Activity Credit)')
+print('1. Menambahkan Kegiatan')
+print('2. Menampilkan Jumlah Poin')
+print('3. Keluar')
+print('---------------------------------')
+a = int(input('Masukan Pilihan : '))
+if a == 1 : 
+   namaitem = input('Nama Kegiatan : ')
+#    index1 = int(input('Masukan Nama Kegiatan : '))
+   namaitem2 = input('Tanggal Kegiatan : ')
+#    index2 = int(input('Masukan Tanggal Kegiatan'))
+   print('Pilihan Kategori Kegiatan : ')
+   print('1 Prestasi')
+   print('2 Organisasi')
+   print('3 Kepanitiaan')
+   print('4 Rekognisi')
+   b = int(input('Masukan Kategori Kegiatan: '))
+   if b == 1 :
+    print('Input Kegiatan Berhasil')
+   elif b == 2 :
+    print('Input Kegiatan Berhasil')
+   elif b == 3 : 
+    print('Input Kegiatan Berhasil')
+   else: 
+    print('Input Kegiatan Berhasil') 
+# elif a == 2 :
+#     print('------------')
+    
+
+     
+
+
+
+
+   
+
+
